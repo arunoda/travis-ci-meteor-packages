a71e84e46798c25e1c36f96f6c2a7b456805e303
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-06-17 15:20:57 +0530

    clean configure script

--- configure.sh
+++ configure.sh
@@ -7,12 +7,6 @@ wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_run
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
-# -- mrt will take care of this
-# #installing meteor
-# git clone https://github.com/meteor/meteor.git ./meteor
-# cd ./meteor
-# git checkout release/0.6.3.1
-# cd ..
 
 #installing meteorite
 npm install -g meteorite
